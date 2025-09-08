367a6723b3a086342002f3e57195fc5f43db5332
DruxAMB <druxambmarketing@gmail.com>
2025-09-08 12:19:20 +0000

    added baseapp baseBuilder allowed address

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -39,5 +39,8 @@ export async function GET() {
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
     }),
+    "baseBuilder": {
+      "allowedAddresses": ["0xe5A519177EAcDeC56A44EED8696cF8288F0Ec4EA"]
+    }
   });
 }
